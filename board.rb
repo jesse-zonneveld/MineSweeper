6ba9ac2152d0ad90a90220fbe738d47c9e172030
jesse-zonneveld <jesse.l.zonneveld@gmail.com>
2020-03-17 10:58:28 -0700

    Add simple reveal method to board.rb

--- board.rb
+++ board.rb
@@ -2,16 +2,22 @@ require "./tile.rb"
 
 class Board
     def initialize
+        @adj_increments = [[-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1]]
         @grid = Array.new(9) { Array.new(9, 0)}
+        @bomb_positions = []
+        @seen_positions = []
         populate
+        
     end
 
-    attr_reader :grid
+    attr_reader :grid, :bomb_positions
 
     def populate
-        @grid.map! do |row|
-            row.map! do |col|
+        @grid.map!.with_index do |row, row_i|
+            row.map!.with_index do |col, col_i|
                 col = Tile.new
+                @bomb_positions << [row_i, col_i] if col.type == "bomb"
+                col
             end
         end
     end
@@ -33,7 +39,63 @@ class Board
             puts "#{row_i} #{row.map { |col| col.flipped }}"
         end
     end
+
+    def [](pos)
+        x, y = pos
+        @grid[x][y]
+    end
+
+    def []=(pos, value)
+        x, y = pos
+        @grid[x][y] = value
+    end
+
+    def reveal(pos)
+        return false if self[pos].type == "bomb" 
+        self[pos].flip
+        @seen_positions << pos unless seen_pos?(pos)
+        current_adj = get_adj(pos)
+        new_adj = []
+        until has_bomb?(new_adj)
+            new_adj = []
+            current_adj.each do |pos|
+                self[pos].flip
+                new_adj += get_adj(pos)
+            end
+            current_adj = new_adj
+        end
+    end
+
+    def get_adj(pos)
+        x, y = pos
+        current_adj = []
+        @adj_increments.each do |increment_pair|
+            new_pos = [x + increment_pair[0], y + increment_pair[1]]
+            current_adj << new_pos if valid_pos?(new_pos) && !seen_pos?(new_pos)
+            @seen_positions << new_pos
+        end
+        current_adj
+    end
+
+    def has_bomb?(arr)
+        (arr & @bomb_positions).any?
+    end
+
+    def valid_pos?(pos)
+        x, y = pos
+        return false if !x.between?(0, 8)
+        return false if !y.between?(0, 8)
+        true
+    end
+
+    def seen_pos?(pos)
+        @seen_positions.include?(pos)
+    end
+
 end
 
 b = Board.new
+b.render_cheat
+p b.bomb_positions
+b.reveal([0,0])
 b.render
